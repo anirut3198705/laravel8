d9e2adcc62525576de6466ef525852cc7760154d
anirut3198705 <95035200+anirut3198705@users.noreply.github.com>
2021-11-30 18:47:19 +0700

    week 2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get("/blog/{id}", function($id) {
+    return "<h1>This is blog page : {$id} </h1>" ;
+});
+Route::get( "/blog/{id}/edit" , function($id) {
+    return "<h1>This is blog page : {$id} for edit</h1>" ;
+});
+Route::get("/product/{a}/{b}/{c}", function($a, $b, $c) {
+    return "<h1>This is product page </h1><div>{$a} , {$b}, {$c}</div>" ;
+});
+
+Route::get("/myorder/create", function() {
+    return "<h1>This is order form ". request("id")." page : ". request("username") ."</h1>" ;
+
+});
+Route::get("/hello", function () {	
+    return view("hello");
+    });
+    Route::get('/greeting', function () {
+
+        $name = 'James';
+    $last_name = 'Mars';
+    
+    return view('greeting', compact('name','last_name') );
+    });
+    
+    
